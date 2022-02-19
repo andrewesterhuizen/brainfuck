caad0578234126b7a1fa4af7a4c93c1e019e9871
Andrew Esterhuizen <andrewesterhuizen@Andrews-MacBook-Pro.local>
2022-02-19 17:25:54 +0000

    Add command line input and source file support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 mod brainfuck;
+use std::env;
+use std::fs;
 
 fn main() {
-    let hello_world = r#"++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++.>+.+++++++..+++.<<++.>+++++++++++++++.>.+++.------.--------.<<+.<."#;
+    let args: Vec<String> = env::args().collect();
+
+    let filename_or_program = args.get(1);
+    if let None = filename_or_program {
+        println!("usage: provide name of .bf file or a brainfuck program");
+        return;
+    }
+
+    let filename_or_program = filename_or_program.unwrap().trim().to_string();
+
+    let source = if filename_or_program.ends_with(".bf") {
+        fs::read_to_string(filename_or_program).expect("error reading file")
+    } else {
+        filename_or_program
+    };
 
     let mut interpreter = brainfuck::Brainfuck::new();
-    interpreter.run(hello_world.to_string());
+    interpreter.run(source);
 }
